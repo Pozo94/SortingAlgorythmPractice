9bdcd1642ee1735fc6f035c15c754fefd53f8d41
jacekpozo@gmail.com <Pozowski94>
2022-01-23 02:07:28 +0100

    Simple sorting algorithms test update

--- src/Algorithms/SortingMethodsTests.java
+++ src/Algorithms/SortingMethodsTests.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SortingMethodsTests {
-    int[]array={2,5,6,4,8,9,1,3,7,0};
-    int[]expArray={0,1,2,3,4,5,6,7,8,9};
+    int[]array={99999,2,5,500,6,123456,4,8,999,9,1,3,7,0,100,10000};
+    int[]expArray={0,1,2,3,4,5,6,7,8,9,100,500,999,10000,99999,123456};
     @Test
     void bubblesortArray(){
         var bubblesort=new Bubblesort(array);
